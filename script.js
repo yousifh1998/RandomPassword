725f080a63183eac40239d23381e5ee40299beec
Yousif Hamami <yousif4502@yahoo.com>
2023-04-17 22:34:45 -0700

    created button function under script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var generateBTN = document.querySelector("#genearate");
+
+function WritePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+}
+
+generateBTN.addEventListener("click", writePassword);
