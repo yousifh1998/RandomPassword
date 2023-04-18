795907a90599a0a1e36f63277fa3591d6dec77ba
Yousif Hamami <yousif4502@yahoo.com>
2023-04-18 15:55:21 -0700

    added css styling to the html page

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var generateBTN = document.querySelector("#genearate");
 
 function generatePassword() {
-  console.log("You clicked the button!")
+  console.log("You clicked the button!");
    
-  return "Generated Password will go here!"
+  return "Generated Password will go here!";
 }
 
 
-     function WritePassword() {
+     function writePassword() {
     var password = generatePassword(); 
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
 }
 
-generateBTN.addEventListener("click", WritePassword);
+generateBTN.addEventListener("click", writePassword);
